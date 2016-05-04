2444924cbb8e1fc43667e3d5e17ba83549c9422a
Johann <johannengrand59@gmail.com>
2016-05-04 18:15:48 +0200

    ADD: Ajout des fichiers game.cpp et game.hpp à compléter bien sur
    Signed-off-by: Johann <johannengrand59@gmail.com>

---
+++ Game.cpp
@@ -0,0 +1,21 @@
+#include "Game.hpp"
+
+public Game::Game(View typeView, Player player1, Player player2);// Il faudrait mettre les arguments passer à la console dans les paramètres
+    void displayGameStatus() const{
+
+    }
+    void playMove() const{
+
+    }
+    bool isValid(){
+
+    }
+    void nextPlayer() const{
+
+    }
+    void endGame() const{
+
+    }
+    void displayResult() const{
+
+    }
---
+++ Game.hpp
@@ -0,0 +1,24 @@
+#ifndef GAME_HPP
+#define GAME_HPP
+
+class Game{
+ private:
+     Player player1;
+     Player player2;
+
+ public:
+    Game(View typeView, Player player1, Player player2); // Constructeur
+    void displayGameStatus() const; //Affiche l'état courant du jeu
+    void playMove() const; //Jouer un coup
+    bool isValid(); // Savoir si le coup est valide
+    void nextPlayer() const; //void ou type Player dépend de ce qu'on en fait -> Permet de passer au joueur suivant
+    void endGame() const ; // Permet d'arreter la partie si la partie est finie
+    void displayResult() const;
+
+};
+
+
+
+
+
+#endif

9244886345c37397bcf2938527fe52d17e977930
Jouin Alexis <alexis.jouin@live.fr>
2016-05-22 21:13:15 +0200

    ADD : menu avec les options de jouabilités dans le main TO DO : commencer les interractions avec le jeu

--- src/MainProg.cpp
+++ src/MainProg.cpp
@@ -6,14 +6,51 @@
 
 
 int main() {
+  int choix;
+  bool gameOn = true;
 
-  //Affichage de 10 nombres aléatoires
-   Game g;
-    std::vector<int> coupslibres = g.getCoupsLibres();
-    for(int c : coupslibres){
-      std::cout << c << " ";
+  Game g;
+  std::vector<int> coupslibres = g.getCoupsLibres();
+
+  //Menu de démarrage avec options de jouabilité
+  while (gameOn == true){
+    std::cout << "Bienvenue dans le jeu du quinze vainc.\n";
+    std::cout <<"1 - Jouer en mode graphique.\n";
+    std::cout <<"2 - Jouer en mode console.\n";
+    std::cout <<"3 - Quitter le jeu.\n";
+    std::cout <<"Entrer votre choix et appuyer sur entrée : ";
+    std::cin >> choix;
+  
+
+    switch (choix){
+    case 1:
+      std::cout << "Mode Graphique lancé.\n";
+      //Rajouté code pour la partie graphique
+      gameOn = false;
+      break;
+
+    case 2:
+      std::cout << "Mode Console lancé\n";
+
+      //Affichage de 10 nombres aléatoires
+      for(int c : coupslibres){
+	std::cout << c << " ";
+      }
+      std::cout << " " << std::endl;
+
+      break;
+    case 3:
+      std::cout << "Arrêt du programme";
+      gameOn = false;
+      break;
+    default:
+      std::cout << "Choix invalide ! \n";
+      std::cout << "Réessayer :\n";
+      std::cin >> choix;
+      break;
     }
-    std::cout << " " << std::endl;
+  }
+  
 
     return 0;
 }
